45b4e217f58e44c2e00ceaaf63c7f59ffaacf2ab
Jim <gjwlals111@naver.com>
2017-03-16 23:24:53 +0900

    emg data to .csv

---
+++ Jim/emgsamples/emg-data-sample.cpp
@@ -0,0 +1,254 @@
+// Copyright (C) 2013-2014 Thalmic Labs Inc.
+// Distributed under the Myo SDK license agreement. See LICENSE.txt for details.
+
+// This sample illustrates how to use EMG data. EMG streaming is only supported for one Myo at a time.
+#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <myo/myo.hpp>
+#include <chrono>
+#include <time.h>
+#include <fstream>
+
+enum {UNKNOWN, FIST, FINGERSPREAD, WAVEIN, WAVEOUT}MYOPOSE;
+
+
+class DataCollector : public myo::DeviceListener {
+public:
+    DataCollector()
+    : emgSamples(), onArm(false), isUnlocked(false), roll_w(0), pitch_w(0), yaw_w(0), currentPose()
+    {
+		openFiles();
+    }
+
+    // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
+    void onUnpair(myo::Myo* myo, uint64_t timestamp)
+    {
+
+        // We've lost a Myo.
+        // Let's clean up some leftover state.
+        emgSamples.fill(0);
+		roll_w = 0;
+		pitch_w = 0;
+		yaw_w = 0;
+		onArm = false;
+		isUnlocked = false;
+    }
+
+	void openFiles()
+	{
+		time_t timestamp = time(0);
+		if (emgFile.is_open()) {
+			emgFile.close();
+		}
+		std::ostringstream emgFileString;
+		emgFileString << "sEMGsamples-" << timestamp << ".csv";
+		emgFile.open(emgFileString.str(), std::ios::out);
+		emgFile << "millis,emg1,emg2,emg3,emg4,emg5,emg6,emg7,emg8,gesture";
+	}
+
+	// onOrientationData() is called whenever the Myo device provides its current orientation, which is represented
+	// as a unit quaternion.
+	void onOrientationData(myo::Myo* myo, uint64_t timestamp, const myo::Quaternion<float>& quat)
+	{
+		using std::atan2;
+		using std::asin;
+		using std::sqrt;
+		using std::max;
+		using std::min;
+
+		// Calculate Euler angles (roll, pitch, and yaw) from the unit quaternion.
+		float roll = atan2(2.0f * (quat.w() * quat.x() + quat.y() * quat.z()),
+			1.0f - 2.0f * (quat.x() * quat.x() + quat.y() * quat.y()));
+		float pitch = asin(max(-1.0f, min(1.0f, 2.0f * (quat.w() * quat.y() - quat.z() * quat.x()))));
+		float yaw = atan2(2.0f * (quat.w() * quat.z() + quat.x() * quat.y()),
+			1.0f - 2.0f * (quat.y() * quat.y() + quat.z() * quat.z()));
+
+		// Convert the floating point angles in radians to a scale from 0 to 18.
+		roll_w = static_cast<int>((roll + (float)M_PI) / (M_PI * 2.0f) * 18);
+		pitch_w = static_cast<int>((pitch + (float)M_PI / 2.0f) / M_PI * 18);
+		yaw_w = static_cast<int>((yaw + (float)M_PI) / (M_PI * 2.0f) * 18);
+	}
+
+	// onPose() is called whenever the Myo detects that the person wearing it has changed their pose, for example,
+	// making a fist, or not making a fist anymore.
+	void onPose(myo::Myo* myo, uint64_t timestamp, myo::Pose pose)
+	{
+		currentPose = pose;
+
+		if (pose != myo::Pose::unknown && pose != myo::Pose::rest) {
+			// Tell the Myo to stay unlocked until told otherwise. We do that here so you can hold the poses without the
+			// Myo becoming locked.
+			myo->unlock(myo::Myo::unlockHold);
+
+			// Notify the Myo that the pose has resulted in an action, in this case changing
+			// the text on the screen. The Myo will vibrate.
+			myo->notifyUserAction();
+		}
+		else {
+			// Tell the Myo to stay unlocked only for a short period. This allows the Myo to stay unlocked while poses
+			// are being performed, but lock after inactivity.
+			myo->unlock(myo::Myo::unlockTimed);
+		}
+	}
+
+	// onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+	// arm. This lets Myo know which arm it's on and which way it's facing.
+	void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection, float rotation,
+		myo::WarmupState warmupState)
+	{
+		onArm = true;
+		whichArm = arm;
+	}
+
+	// onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+	// it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+	// when Myo is moved around on the arm.
+	void onArmUnsync(myo::Myo* myo, uint64_t timestamp)
+	{
+		onArm = false;
+	}
+
+	// onUnlock() is called whenever Myo has become unlocked, and will start delivering pose events.
+	void onUnlock(myo::Myo* myo, uint64_t timestamp)
+	{
+		isUnlocked = true;
+	}
+
+	// onLock() is called whenever Myo has become locked. No pose events will be sent until the Myo is unlocked again.
+	void onLock(myo::Myo* myo, uint64_t timestamp)
+	{
+		isUnlocked = false;
+	}
+
+    // onEmgData() is called whenever a paired Myo has provided new EMG data, and EMG streaming is enabled.
+    void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg)
+    {
+        for (int i = 0; i < 8; i++) {
+            emgSamples[i] = emg[i];
+        }
+    }
+
+    // There are other virtual functions in DeviceListener that we could override here, like onAccelerometerData().
+    // For this example, the functions overridden above are sufficient.
+
+    // We define this function to print the current values that were updated by the on...() functions above.
+    void print()
+    {
+		static std::chrono::system_clock::time_point StartTime = std::chrono::system_clock::now();
+		std::chrono::system_clock::time_point EndTime = std::chrono::system_clock::now();
+		std::chrono::milliseconds mill = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime);
+		
+
+		
+        // Clear the current line
+		emgFile << std::endl;
+		emgFile << mill.count();
+
+        // Print out the EMG data.
+        for (size_t i = 0; i < emgSamples.size(); i++) {
+            std::ostringstream oss;
+            oss << static_cast<int>(emgSamples[i]);
+            std::string emgString = oss.str();
+
+            emgFile << ',' << emgString;
+        }
+		if (onArm)
+		{
+			std::string poseString = currentPose.toString();
+			if (poseString == "fist")
+				MYOPOSE = FIST;
+			else if (poseString == "fingersSpread")
+				MYOPOSE = FINGERSPREAD;
+			else if (poseString == "waveIn")
+				MYOPOSE = WAVEIN;
+			else if (poseString == "waveOut")
+				MYOPOSE = WAVEOUT;
+			else
+				MYOPOSE = UNKNOWN;
+			emgFile << ',' << MYOPOSE;
+		}
+		else
+		{
+			MYOPOSE = UNKNOWN;
+			emgFile << MYOPOSE;
+		}
+		std::cout << std::endl;
+        std::cout << std::flush;
+		
+    }
+
+    // The values of this array is set by onEmgData() above.
+    std::array<int8_t, 8> emgSamples;
+
+	// These values are set by onArmSync() and onArmUnsync() above.
+	bool onArm;
+	myo::Arm whichArm;
+
+	// This is set by onUnlocked() and onLocked() above.
+	bool isUnlocked;
+
+	// These values are set by onOrientationData() and onPose() above.
+	int roll_w, pitch_w, yaw_w;
+	myo::Pose currentPose;
+	std::ofstream emgFile;
+};
+
+int main(int argc, char** argv)
+{
+    // We catch any exceptions that might occur below -- see the catch statement for more details.
+    try {
+
+    // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+    // publishing your application. The Hub provides access to one or more Myos.
+    myo::Hub hub("com.example.emg-data-sample");
+
+    std::cout << "Attempting to find a Myo..." << std::endl;
+
+    // Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
+    // immediately.
+    // waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
+    // if that fails, the function will return a null pointer.
+    myo::Myo* myo = hub.waitForMyo(10000);
+
+    // If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+    if (!myo) {
+        throw std::runtime_error("Unable to find a Myo!");
+    }
+
+    // We've found a Myo.
+    std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+
+    // Next we enable EMG streaming on the found Myo.
+    myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+
+    // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+    DataCollector collector;
+
+    // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+    // Hub::run() to send events to all registered device listeners.
+    hub.addListener(&collector);
+
+    // Finally we enter our main loop.
+    while (1) {
+        // In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+        // In this case, we wish to update our display 50 times a second, so we run for 1000/20 milliseconds.
+        hub.run(1000/20);
+        // After processing events, we call the print() member function we defined above to print out the values we've
+        // obtained from any events that have occurred.
+        collector.print();
+    }
+
+    // If a standard exception occurred, we print out its message and exit.
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Press enter to continue.";
+        std::cin.ignore();
+        return 1;
+    }
+}
