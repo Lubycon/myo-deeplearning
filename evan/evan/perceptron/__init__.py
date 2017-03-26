19c3dc032362dba22c7c35f4e854b2596d72c422
Evan-Macbook-pro <bboydart91@gmail.com>
2017-03-26 13:45:47 +0900

    matrix dot method is updated to generally logic

--- evan/evan/perceptron/__init__.py
+++ evan/evan/perceptron/__init__.py
@@ -22,7 +22,6 @@ def run():
 
     gate = gc.GateGenerator(w1, w2)
 
-    gate.XOR(x1, x2)
     logging.debug('\n x1=%d\n x2=%d\n w1=%.2f\n w2=%.2f\n'%(x1, x2, w1, w2))
     logging.debug('   AND(%d)  NAND(%d)  OR(%d)  XOR(%d)   '%(gate.AND(x1, x2), gate.NAND(x1, x2), gate.OR(x1, x2), gate.XOR(x1, x2)))
     logging.debug('======================================================')
@@ -30,10 +29,13 @@ def run():
 # for i in range(0, limit):
 #     run()
 
-m1 = np.ones((3,3))
-m2 = np.eye(3)
+m1 = np.array([[1.0,2.0,3.0],[4.0,5.0,6.0]])
+m2 = np.array([[6.0,5.0,4.0],[3.0,2.0,1.0]])
 
 matrix = mt.MatrixClass(m1, m2)
-print('add =>', matrix.add())
+print(m1)
+print(m2)
+print('=====================')
+print('add =>' ,matrix.add())
 print('sub =>', matrix.sub())
 print('dot =>', matrix.dot())
--- evan/evan/perceptron/matrix.py
+++ evan/evan/perceptron/matrix.py
@@ -9,17 +9,35 @@ class MatrixClass:
         m1 = self.m1
         m2 = self.m2
 
+        m1Size = self.sizeCal(m1)
+        m2Size = self.sizeCal(m2)
 
+        if self.sizeMatchValidator(m1, m2, 'dot') != True:
+            return 'Matrix size Error'
+
+        m3 = np.zeros((m1Size['row'], m2Size['col']))
+
+        for i in range(m1Size['row']):
+            for j in range(m2Size['col']):
+                result = 0
+
+                for k in range(m2Size['row']):
+                    result += m1[i][k] * m2[k][j]
+
+                m3[i][j] = result
+
+        return m3
 
 
     def add(self):
         m1 = self.m1
         m2 = self.m2
 
-        if self.sizeMatchValidator(m1, m2) != True:
-            return 'matrix size Error'
+        if self.sizeMatchValidator(m1, m2, 'add') != True:
+            return 'Matrix size Error'
 
-        m3 = np.zeros((len(m1[0]), len(m1)))
+        size = self.sizeCal(m1)
+        m3 = np.zeros((size['row'], size['col']))
 
         for i,v in enumerate(m1):
             for j,vv in enumerate(v):
@@ -31,10 +49,11 @@ class MatrixClass:
         m1 = self.m1
         m2 = self.m2
 
-        if self.sizeMatchValidator(m1, m2) != True:
-            return 'matrix size Error'
+        if self.sizeMatchValidator(m1, m2, 'sub') != True:
+            return 'Matrix size Error'
 
-        m3 = np.zeros((len(m1[0]), len(m1)))
+        size = self.sizeCal(m1)
+        m3 = np.zeros((size['row'], size['col']))
 
         for i,v in enumerate(m1):
             for j,vv in enumerate(v):
@@ -42,5 +61,19 @@ class MatrixClass:
 
         return m3
 
-    def sizeMatchValidator(self, m1, m2):
-        return len(m1) == len(m2) and len(m1[0]) == len(m2[0])
+    def sizeMatchValidator(self, m1, m2, kind):
+        size1 = self.sizeCal(m1)
+        size2 = self.sizeCal(m2)
+
+        if kind == 'add' or kind == 'sub':
+            return size1['row'] == size2['row'] and size1['col'] == size2['col']
+        elif kind == 'dot':
+            return size1['row'] == size2['col']
+
+
+
+    def sizeCal(self, m):
+        return {
+            'row': len(m),
+            'col': len(m[0])
+        }
