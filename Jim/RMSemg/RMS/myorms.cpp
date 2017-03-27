ab261ce29ac514c59fa5872afe592f43af6c9a09
Jim <gjwlals111@naver.com>
2017-03-27 13:03:47 +0900

    RMS invalid

--- Jim/RMSemg/RMS/myorms.cpp
+++ Jim/RMSemg/RMS/myorms.cpp
@@ -6,28 +6,29 @@
 class Filter{
 private:
 	int SIZE;
-	int prefilter;
+	double prefilter;
+	double pre_front;
 public:
 	Filter(int size);
-	int movingavgfilter(int newval, std::queue<int>* Q);
+	double movingavgfilter(int newval, std::queue<int>* Q);
 };
 
 Filter::Filter(int size)
 {
+	pre_front = 0;
 	prefilter = 0;
 	SIZE = size;
 }
 
-int Filter::movingavgfilter(int newval, std::queue<int>* Q)
+double Filter::movingavgfilter(int newval, std::queue<int>* Q)
 {
-	int newfilter;
+	double newfilter = 0.0;
 	int sumval = 0;
 	std::queue<int> tmpQ;
-	static int pre_front = 0;
 
 	if (newval < 0)
 		newval *= -1;
-
+	///*
 	Q->push(newval);
 
 	if (Q->size() != SIZE)
@@ -41,20 +42,37 @@ int Filter::movingavgfilter(int newval, std::queue<int>* Q)
 			sumval += tmpQ.front();
 			tmpQ.pop();
 		}
-		newfilter = sumval / SIZE;
+		newfilter = sumval / (double)SIZE;
 	}
 	else
-		newfilter = prefilter + (newval - pre_front) / SIZE;
+		newfilter = prefilter + (newval - pre_front) / (double)SIZE;
 
 
-	if (Q->size() == SIZE)
-	{
-		pre_front = Q->front();
-		Q->pop();
-	}
+	
+	pre_front = Q->front();
+	Q->pop();
+
 
 	prefilter = newfilter;
+	//*/
+	/*
+	Q->push(newval);
+	tmpQ = *Q;
 
+	if (tmpQ.size() != SIZE)
+		sumval = tmpQ.front() * (SIZE - tmpQ.size());
+
+	while (!tmpQ.empty())
+	{
+		sumval += tmpQ.front();
+		tmpQ.pop();
+	}
+	newfilter = sumval / (double)SIZE;
+
+	if (Q->size() == SIZE)
+		Q->pop();
+	prefilter = newfilter;
+	*/
 	return newfilter;
 }
 
@@ -65,7 +83,7 @@ int main(int argc, char** argv)
 	std::ofstream outFile;
 	std::queue<int> msQ, labelQ;
 	std::queue<int> emgQ[8];
-	std::queue<int> RMSemgQ[8];
+	std::queue<double> RMSemgQ[8];
 	Filter filter(10);
 
 	int tmpms, tmplabel;
@@ -75,7 +93,7 @@ int main(int argc, char** argv)
 	if (inFile.is_open()) {
 		inFile.close();
 	}
-	inFile.open("C:\\Users\\gjwla\\Documents\\GitHub\\study\\data\\myo\\sEMGsamples(waveout)-1490341675.csv", std::ios::in);
+	inFile.open("C:\\Users\\gjwla\\Documents\\GitHub\\study\\data\\myo\\sEMGsamples(wavein)-1490341362.csv", std::ios::in);
 	while (inFile.get(delim))
 		if (delim == '\n')
 			break;
@@ -98,7 +116,7 @@ int main(int argc, char** argv)
 	if (outFile.is_open()) {
 		outFile.close();
 	}
-	outFile.open("sEMGsamples(waveout)-1490341675RMS.csv", std::ios::out);
+	outFile.open("sEMGsamples(wavein)-1490341362RMS.csv", std::ios::out);
 	
 	while (!msQ.empty())
 	{
