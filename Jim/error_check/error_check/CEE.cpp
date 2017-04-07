ba0af6c66bd4e28f0ab5d94a85ed3ec5f99ac758
Jim <gjwlals111@naver.com>
2017-04-07 12:45:56 +0900

    cee 모르겠다...

--- Jim/error_check/error_check/CEE.cpp
+++ Jim/error_check/error_check/CEE.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 #include <vector>
 #include <cmath>
+#include "multilayer.h"
 
 int main(int argc, char **argv)
 {
 	std::ifstream err_data;
-	double predict_val = 4;
+	double predict_val = 0.1;
 	double esti_val;
 	char delim;
 	double sum = 0;
 	double result;
+	std::queue<double>esti_Q;
 	std::vector<double> esti;
-	std::vector<double> predict;
+	std::vector<double> predict = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+	Layer lay(0);
 
 	if (err_data.is_open()) {
 		err_data.close();
@@ -24,10 +28,15 @@ int main(int argc, char **argv)
 		err_data >> esti_val >> delim;
 		esti_Q.push(esti_val);
 	}
+	size_t i = 0;
 	while (!esti_Q.empty())
 	{
-		sum += (log(predict_val) / log(2)); //밑이 2인 로그함수
-		//esti_Q.pop();
+		sum += log(lay.sigmoid(esti_Q.front())) * predict[i];
+		esti_Q.pop();
+		if (i < predict.size() - 1)
+			i++;
+		else
+			i = 0;
 	}
 	result = -sum;
 
